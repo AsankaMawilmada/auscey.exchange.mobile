cb69e1386902fadc35295073fb018168acd42000
Asanka Mawilmada <amawilmada@hotmail.com>
2025-10-16 21:52:32 +1100

    workflow

--- app.config.js
+++ app.config.js
@@ -3,6 +3,9 @@ import "dotenv/config";
 export default ({ config }) => ({
   ...config,
   extra: {
+    eas: {
+      projectId: "ee6749d1-4061-4f9f-84a1-fb7f42e04cd7",
+    },
     API_URL: process.env.API_URL || "https://api.example.com",
     ENV_NAME: process.env.ENV_NAME || "",
   },
